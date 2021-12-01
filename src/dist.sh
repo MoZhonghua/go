91aedd32fe26ed7b0161f5c286776120ed46b058
MoZhonghua <Kasulle@gmail.com>
2021-12-01 16:23:14 +0800

    update

--- src/dist.sh
+++ src/dist.sh
@@ -2,17 +2,17 @@
 
 set -e
 
-env GO111MODULE=off GOROOT="" /usr/bin/go build -o dist .
+env GO111MODULE=off GOROOT="" /usr/bin/go build -o ./cmd/dist/dist ./cmd/dist/
 
 echo "build dist ok"
 
 unset GOBIN
+unset GOPATH
 export GOENV=off
-export GOPATH=/home/mozhonghua/go
 export GOROOT=/home/mozhonghua/go/src/github.com/golang/go
 export GOROOT_BOOTSTRAP=/usr/lib/go
 export GO_LDSO=/lib64/ld-linux-x86-64.so.2
-export GOMAXPROCS=1
+#export GOMAXPROCS=1
 export PATH=/home/mozhonghua/go/src/github.com/golang/go/bin:$PATH
 
-./dist "$@"
+./cmd/dist/dist "$@"
