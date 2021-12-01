72641561eea6232654927b274f334b3c2cfdaaf6
MoZhonghua <Kasulle@gmail.com>
2021-12-01 16:25:21 +0800

    update

--- src/dist.sh
+++ src/dist.sh
@@ -2,17 +2,24 @@
 
 set -e
 
-env GO111MODULE=off GOROOT="" /usr/bin/go build -o ./cmd/dist/dist ./cmd/dist/
+function dist() {
+	env GO111MODULE=off GOROOT="" /usr/bin/go build -o ./cmd/dist/dist ./cmd/dist/
 
-echo "build dist ok"
+	echo "build dist ok"
+	unset GOBIN
+	unset GOPATH
+	export GOENV=off
+	export GOROOT=/home/mozhonghua/go/src/github.com/golang/go
+	export GOROOT_BOOTSTRAP=/usr/lib/go
+	export GO_LDSO=/lib64/ld-linux-x86-64.so.2
+	#export GOMAXPROCS=1
+	export PATH=/home/mozhonghua/go/src/github.com/golang/go/bin:$PATH
 
-unset GOBIN
-unset GOPATH
-export GOENV=off
-export GOROOT=/home/mozhonghua/go/src/github.com/golang/go
-export GOROOT_BOOTSTRAP=/usr/lib/go
-export GO_LDSO=/lib64/ld-linux-x86-64.so.2
-#export GOMAXPROCS=1
-export PATH=/home/mozhonghua/go/src/github.com/golang/go/bin:$PATH
+	./cmd/dist/dist "$@"
+}
 
-./cmd/dist/dist "$@"
+SELF_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+
+(
+	cd $SELF_DIR && dist "$@"
+)
