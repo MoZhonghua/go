c48af0147da6fc1e540b3ca800d34d04f0c2aa5f
MoZhonghua <Kasulle@gmail.com>
2021-12-20 17:32:10 +0800

    update

---
+++ src/go.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+set -e
+
+function buildgo() {
+	export PATH=/home/mozhonghua/go/src/github.com/golang/go/bin:$PATH
+	export GOROOT=/home/mozhonghua/go/src/github.com/golang/go
+
+	cp ../bin/{go.old,go}
+	go build -a -o ../bin/go2 ./cmd/go/
+	mv -f ../bin/{go2,go}
+
+	echo "------build go ok-------------"
+
+	../bin/go "$@"
+}
+
+SELF_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+
+(
+	cd $SELF_DIR && buildgo version
+)
