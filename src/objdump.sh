dfbda2e04e950cc64cb90e37670517192d60770d
MoZhonghua <Kasulle@gmail.com>
2021-12-20 15:22:31 +0800

    update

--- src/objdump.sh
+++ src/objdump.sh
@@ -6,13 +6,13 @@ function objdump() {
 	export PATH=/home/mozhonghua/go/src/github.com/golang/go/bin:$PATH
 	export GOROOT=/home/mozhonghua/go/src/github.com/golang/go
 
-	go build -o ../pkg/tool/linux_amd64/objdump2 ./cmd/objdump/
+	go build -o ../pkg/tool/linux_amd64/objdump ./cmd/objdump/
 
-	../pkg/tool/linux_amd64/objdump2 "$@"
+	../pkg/tool/linux_amd64/objdump "$@"
 }
 
 SELF_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 
 (
-	cd $SELF_DIR && objdump
+	cd $SELF_DIR && objdump "$@"
 )
